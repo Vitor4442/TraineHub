921f56c4f4f4f5f6e4c0cc4cec2d0ee6fb63395b
José Vitor <jose.vitorw018@gmail.com>
2026-01-31 00:18:22 -0300

    configurando o swagger

---
+++ exercises/src/main/java/com/vtr/exercises/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.vtr.exercises.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST API's RESTful from 0 with Java, Spring Boot, Kubertenes and Docker")
+                        .version("v1")
+                        .description("REST API's RESTful from 0 with Java, Spring Boot, Kubertenes and Docker")
+                        .termsOfService("e meu")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("link"))
+                );
+    }
+}

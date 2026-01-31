b5304dd5e61001898f02c1c360658195e39b4402
José Vitor <jose.vitorw018@gmail.com>
2026-01-31 17:20:42 -0300

    Feito os content negation

--- exercises/src/main/java/com/vtr/exercises/controller/StrudentController.java
+++ exercises/src/main/java/com/vtr/exercises/controller/StrudentController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,10 @@ public class StrudentController implements StrudentControllerDocs {
 
     private final StudentService service;
 
-    @PostMapping("/cadastros")
+    @PostMapping(
+            produces = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            value = "/cadastros" )
     @Override
     public ResponseEntity<StudentDTO> addStudent(@RequestBody Student student){
         StudentDTO savedStudent = service.addStudent(student);
@@ -32,26 +36,32 @@ public class StrudentController implements StrudentControllerDocs {
     }
 
 
-    @GetMapping
+    @GetMapping(
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     @Override
     public ResponseEntity<List<StudentDTO>> findAllStudents(){
         return ResponseEntity.ok(service.findAllStudents());
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(produces = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            value = "{id}"
+    )
     @Override
     public ResponseEntity<StudentDTO> putStudent(@RequestBody StudentDTO studentDTO, @PathVariable Long id) {
         return ResponseEntity.ok( service.putStudent(id, studentDTO));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(produces = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE },
+            value = "{id}")
     @Override
     public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
         service.deleteStudent(id);
         return ResponseEntity.noContent().build(); // HTTP 204
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE }, value = "/{id}")
     @Override
     public ResponseEntity<StudentDTO> findByIdStudent(@PathVariable Long id){
         StudentDTO student = service.findByIdStudent(id);

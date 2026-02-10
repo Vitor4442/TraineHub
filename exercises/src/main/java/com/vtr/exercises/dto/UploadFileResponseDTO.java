486d2c2bb2646cbbf96d6f1dec2681e4a0d01391
José Vitor <jose.vitorw018@gmail.com>
2026-02-10 20:58:05 -0300

    Definindo as Exceptions e o DTO de Response

---
+++ exercises/src/main/java/com/vtr/exercises/dto/UploadFileResponseDTO.java
@@ -0,0 +1,22 @@
+package com.vtr.exercises.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UploadFileResponseDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+
+}

f61bcb04ac88d815d5bd64f1d741ba08cf90d5b7
José Vitor <jose.vitorw018@gmail.com>
2026-02-11 22:43:11 -0300

    Feito o upload de arquivos

--- exercises/src/main/java/com/vtr/exercises/controller/FileController.java
+++ exercises/src/main/java/com/vtr/exercises/controller/FileController.java
@@ -6,22 +6,28 @@ import com.vtr.exercises.service.FileStorageService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/fichas ")
+@RequestMapping("/fichas")
 @RequiredArgsConstructor
 public class FileController implements FileControllerDocs {
 
     private final FileStorageService service;
 
+    @PostMapping("/uploadFile")
     @Override
-    public UploadFileResponseDTO uploadFile(MultipartFile file) {
-        return null;
+    public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+        var fileName = service.storeFile(file);
+        var fileDownloadURI = ServletUriComponentsBuilder.fromCurrentContextPath().path("/fichas/downlaodFicha/").path(fileName).toUriString();
+        return new UploadFileResponseDTO(fileName, fileDownloadURI, file.getContentType(), file.getSize());
     }
 
     @Override

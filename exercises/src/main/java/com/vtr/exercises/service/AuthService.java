fa9bf18d568965d0aeb8f11bd6dd966cb4e6dd19
José Vitor <jose.vitorw018@gmail.com>
2026-02-17 14:14:12 -0300

    Feito o auth service

---
+++ exercises/src/main/java/com/vtr/exercises/service/AuthService.java
@@ -0,0 +1,39 @@
+package com.vtr.exercises.service;
+
+import com.vtr.exercises.dto.security.AccountCredentialsDTO;
+import com.vtr.exercises.dto.security.TokenDTO;
+import com.vtr.exercises.repository.PersonalUserRepository;
+import com.vtr.exercises.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+    private final PersonalUserRepository repository;
+
+    public ResponseEntity<TokenDTO> singIn(AccountCredentialsDTO credentials){
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    credentials.getEmail(),
+                    credentials.getPassword()
+            )
+        );
+
+        var user = repository.findByUserEmail(credentials.getEmail());
+        if(user == null) throw new UsernameNotFoundException("Email " + credentials.getEmail() + " not found");
+
+        var tokenResponse = tokenProvider.createAcessToken(
+                credentials.getEmail(),
+                user.getRoles()
+        );
+
+        return ResponseEntity.ok(tokenResponse);
+    }
+}
